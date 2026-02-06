8e38e79efac6d0d1bd5e3812ccb9f0e7ecde2f43
adwaid20 <adwaidajayantc22@gmail.com>
2026-02-06 12:10:44 +0530

    initial commit

---
+++ anagram/main.py
@@ -0,0 +1,16 @@
+# x=str(input("enter the first word"))
+# y=str(input("enter the second word"))
+
+# def anagram(a,b):
+#     a=a.replace(" ","").lower()
+#     b=b.replace(" ","").lower()
+#     return sorted(a)==sorted(b)
+    
+# if anagram(x,y):
+#     print("it is anagram")
+# else:
+#     print("not anagram")
+
+x="adwaid AJAYAan"
+x=x.replace(" ","").lower()
+print(x)
